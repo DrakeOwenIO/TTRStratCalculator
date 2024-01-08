26ace5903f48298d6a49f7edeb8f30653f605295
Drake Owen <[drake@reidfarms.com]>
2024-01-08 00:35:09 -0500

    Imported gag data and generated attack list

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,40 @@
 console.log("JS Loaded");
 
-function parseJSON(dataFile) {
-    
+import GAGDATA from '/src/JSON/gags.json' assert {type: 'json'};
+
+console.log(GAGDATA);
+
+function CreateGagList(GAGDATA) {
+    const allgags = [];
+
+    // Iterate over each gag class and its associated gags
+    for (const gagType in GAGDATA) {
+        if (GAGDATA.hasOwnProperty(gagType)) {
+
+            // Exclude Lure and Toonup
+            if (gagType.toLowerCase() === "toonup" || gagType.toLowerCase() === "lure") {
+                continue;
+            }
+            const gags = GAGDATA[gagType];
+
+            for (const gagName in gags) {
+                if (gags.hasOwnProperty(gagName)) {
+                    const gagDetails = gags[gagName];
+                    allgags.push({
+                        gagType: gagType,
+                        gagName: gagName,
+                        minDamage: gagDetails[0],
+                        maxDamage: gagDetails[1],
+                        orgDamage: gagDetails[2],
+                        accuracy: gagDetails[3]
+                    });
+                }
+            }
+        }
+    }
+    return allgags;
 }
+
+const GAGLIST = CreateGagList(GAGDATA);
+
+console.log(GAGLIST);
